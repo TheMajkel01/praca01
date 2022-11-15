207e76486c6fc4bac5231d498e06208d42db07e6
TheMajkel01 <michalnl@onet.pl>
2022-11-15 23:22:50 +0100

    text added (no code yet)

--- app/src/main/java/com/example/praca1/MainActivity.kt
+++ app/src/main/java/com/example/praca1/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.praca1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,5 +47,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+
     }
 }
